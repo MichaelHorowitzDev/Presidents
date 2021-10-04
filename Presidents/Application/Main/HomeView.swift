d982a146b4f60c53c6aaa03ee78d0f3444656dad
MichaelHorowitzDev <michaelhorowitzdev@gmail.com>
2021-10-04 00:47:21 -0400

    added home view

---
+++ Presidents/Application/Main/HomeView.swift
@@ -0,0 +1,40 @@
+//
+//  HomeView.swift
+//  Presidents
+//
+//  Created by Michael Horowitz on 10/4/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+      NavigationView {
+        GeometryReader { geo in
+          List(0..<presidentNames.count, id: \.self) { name in
+            NavigationLink {
+              Text(presidentNames[name])
+            } label: {
+              HStack {
+                Image("\(name+1)_Preview")
+                  .resizable()
+                  .scaledToFit()
+                  .clipShape(Circle())
+                Spacer()
+                Text(presidentNames[name])
+                  .font(.system(size: min(geo.size.height, geo.size.width) * 0.05))
+              }
+              .frame(maxHeight: 100)
+            }
+          }
+        }
+        .navigationTitle("Presidents")
+      }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
