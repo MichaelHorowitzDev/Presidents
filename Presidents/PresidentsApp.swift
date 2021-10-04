ea2a3e52d4d7434556b250bba5312c7b2b7f62a3
MichaelHorowitzDev <michaelhorowitzdev@gmail.com>
2021-10-03 23:51:05 -0400

    Initial Commit

---
+++ Presidents/PresidentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresidentsApp.swift
+//  Presidents
+//
+//  Created by Michael Horowitz on 10/3/21.
+//
+
+import SwiftUI
+
+@main
+struct PresidentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
