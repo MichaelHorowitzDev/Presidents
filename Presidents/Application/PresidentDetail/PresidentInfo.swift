d25051b5a2d64f109cafb36beac5277162e7ac54
MichaelHorowitzDev <michaelhorowitzdev@gmail.com>
2021-10-06 01:19:47 -0400

    President Info View

--- Presidents/Application/PresidentDetail/PresidentInfo.swift
+++ Presidents/Application/PresidentDetail/PresidentInfo.swift
@@ -37,17 +37,22 @@ struct PresidentInfo: View {
     Group {
       PresidentInfoView(title: "Born", text: presidentDetail.born)
       PresidentInfoView(title: "Died", text: presidentDetail.died)
+      PresidentInfoView(title: "Start Term", text: presidentDetail.startTerm)
+      PresidentInfoView(title: "End Term", text: presidentDetail.endTerm)
+      PresidentInfoView(title: "Parents", text: presidentDetail.parents)
+      PresidentInfoView(title: presidentDetail.spouses?.count == 1 ? "Spouse" : "Spouses", text: presidentDetail.spouses)
+      PresidentInfoView(title: "Religion", text: presidentDetail.religion)
+      PresidentInfoView(title: "Political Party", text: presidentDetail.politicalParty)
+      PresidentInfoView(title: "Career", text: presidentDetail.career)
+      PresidentInfoView(title: "Nickname", text: presidentDetail.nickname)
+    }
+    Group {
       PresidentInfoView(title: "Birth Place", text: presidentDetail.birthPlace)
       PresidentInfoView(title: "Location Died", text: presidentDetail.locationDied)
       PresidentInfoView(title: "Cause of Death", text: presidentDetail.causeOfDeath)
       PresidentInfoView(title: "Resting Place", text: presidentDetail.restingPlace)
-      PresidentInfoView(title: "Political Party", text: presidentDetail.politicalParty)
-      PresidentInfoView(title: "Parents", text: presidentDetail.parents)
-      PresidentInfoView(title: presidentDetail.spouses?.count == 1 ? "Spouse" : "Spouses", text: presidentDetail.spouses)
-    }
-    Group {
-      PresidentInfoView(title: "Start Term", text: presidentDetail.startTerm)
-      PresidentInfoView(title: "End Term", text: presidentDetail.endTerm)
+      
+      
     }
   }
 }
